1f502bbcc1476c8040395dd75f8ad72b64faa416
Shivakumaraulli <113375129+Shivakumaraulli@users.noreply.github.com>
2023-05-01 12:14:56 +0530

    Add files via upload

---
+++ React Handson 7/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css'
+import HoverCom from './Components/HoverCom'
+import PureCom from './Components/PureCom'
+
+function App() {
+  return (
+    <div className="App">
+      <HoverCom/>
+      <hr />
+      <PureCom/>
+      
+    </div>
+  );
+}
+
+export default App;
